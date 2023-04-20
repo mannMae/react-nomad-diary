706278602d892adddd02ad2c1c78287660f0c133
김재만 <daga4242@gmail.com>
2023-04-20 22:09:39 +0900

    feat: read from useContext and add realm Listener

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
+import { useDB } from "../context";
+import { FlatList } from "react-native";
 
 const View = styled.View`
   flex: 1;
@@ -29,10 +31,54 @@ const Button = styled.TouchableOpacity`
 
 const ButtonText = styled.Text``;
 
+const Record = styled.View`
+  background-color: #000;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const Emotion = styled.Text`
+  font-size: 20px;
+  margin-right: 10px;
+`;
+
+const Message = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  font-weight: 500;
+`;
+
+const Separator = styled.View`
+  height: 10px;
+`;
+
 export const Home = ({ navigation: { navigate } }) => {
+  const realm = useDB();
+  const [feelings, setFeelings] = useState(realm.objects("Feeling"));
+  useEffect(() => {
+    const feelings = realm.objects("Feeling");
+    feelings.addListener(() => {
+      const feelings = realm.objects("Feeling");
+      setFeelings(feelings);
+    });
+  }, []);
   return (
     <View>
       <Title>My Journal</Title>
+      <FlatList
+        data={feelings}
+        keyExtractor={(feeling) => feeling._id + ""}
+        ItemSeparatorComponent={<Separator />}
+        contentContainerStyle={{ paddingVertical: 10 }}
+        renderItem={({ item }) => (
+          <Record>
+            <Emotion>{item.emotion}</Emotion>
+            <Message>{item.message}</Message>
+          </Record>
+        )}
+      />
       <Button onPress={() => navigate("Write")}>
         <Ionicons name="add" color="tomato" size={40} />
       </Button>
